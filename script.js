75699abda8b04c28db01bd0a33772e8520be0ec9
Daniel_KP <yourmakerz@gmail.com>
2024-12-02 12:42:06 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
